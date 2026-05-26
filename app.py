71c212525273672ebd45fbfbf5e54f14cb552ada
Dibyansh Mohanty <mohantydibyansh77@gmail.com>
2026-05-26 22:52:22 +0530

    Add file uploader for TXT files and implement note display in app.py

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ st.title("AI Notes Summarizer")
 
 notes = st.text_area("Paste your notes here")
 
+uploaded_file = st.file_uploader(
+    "Upload a TXT file",
+    type=["txt"]
+)
+
+if uploaded_file is not None:
+
+    notes = uploaded_file.read().decode("utf-8")
+    st.text_area("Uploaded Notes", notes, height=200)
+
 summary_type = st.selectbox(
     "Choose Summary Type",
     [

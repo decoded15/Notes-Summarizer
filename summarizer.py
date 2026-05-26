fd57dcdee235a362d6eb81b9aea2851c4869e820
Dibyansh Mohanty <mohantydibyansh77@gmail.com>
2026-05-26 22:01:30 +0530

    Implement note summarization functionality using Gemini API

---
+++ summarizer.py
@@ -0,0 +1,24 @@
+from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+def summarize_notes(notes):
+    prompt = f"""
+    Summarize these notes into simple bullet points:
+
+    {notes}
+    """
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
+    return response.text
+
+summary = summarize_notes(notes)
